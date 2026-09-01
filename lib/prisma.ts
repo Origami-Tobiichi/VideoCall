d1b88d9a99405105c74f224df8eb548d93464ec5
Origami-Tobiichi <cyberdark454@gmail.com>
2026-09-01 13:56:26 +0800

    Add DATABASE_URL check in prisma.ts
    Added a check for DATABASE_URL and a warning if not set.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,11 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+// Cek apakah DATABASE_URL tersedia
+if (!process.env.DATABASE_URL) {
+  console.warn('⚠️ DATABASE_URL not set. Prisma will not work.');
+}
+
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
